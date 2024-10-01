8134d141fa3d35ff144a2815aac56654f9926441
Daniel Amendoeira <daniel@amendoeira.eu>
2024-10-01 16:09:47 +0100

    config split - create config folder and place line count on filename

--- ftnt-split-config.sh
+++ ftnt-split-config.sh
@@ -4,7 +4,15 @@ B="$1"
 
 #grep -Ee ^config "$1"
 #echo ###
+mkdir -p "${B%.conf}"
+
 grep -Ee ^config "$B" | grep -vEe "^config vdom$" | sed "s/^config //" | while read A ; do
-  echo "$A"
-  sed -n "/^config ${A}$/,/^end/p" "$B" > ${B%.conf}-${A// /-}.conf
+  echo -ne "\033[2K\r"
+  echo -n "$A"
+  TARGET="${B%.conf}"/"${B%.conf}"-"${A// /-}"
+  sed -n "/^config ${A}$/,/^end/p" "$B" > "${TARGET}".conf
+  LINECOUNT=$(wc -l "${TARGET}".conf | cut -f1 -d" ")
+  mv "${TARGET}".conf "${TARGET}"-${LINECOUNT}.conf
 done
+
+echo -ne "\033[2K\r"
